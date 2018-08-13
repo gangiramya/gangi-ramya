86d8d267bc9842a7f8418bc9a72190b9b22fd823
gangiramya <41796378+gangiramya@users.noreply.github.com>
2018-08-13 10:03:42 +0530

    Update even.py

--- even.py
+++ even.py
@@ -1,6 +1,7 @@
 num = int(input("enter a number"))
-mod = num%2
-if mod>o;
-    print("this is an odd number")
-else
-    print("this is an even number")
+if num<0:
+    print("invalid")
+elif(num%2)==0:
+    print("even")
+else:
+    print("odd")
