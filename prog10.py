405d6451a9b964f6a4528cbd67d4edf3e59284ea
gangiramya <41796378+gangiramya@users.noreply.github.com>
2018-08-16 10:48:26 +0530

    Update prog10.py

--- prog10.py
+++ prog10.py
@@ -1,6 +1,13 @@
-n=int(input(" "))
-count=0
-while(n>0):
-    count=count+1
-    n=n//10
-print("count")
+
+a=[1,2,3,4,5,6,7,8,9]
+sum=0
+n=0
+i=0
+k=0
+n = int(input(""))
+k = int(input(""))
+for i in range(0,n):
+    print(a[i])
+for i in range(0,k):
+    sum = sum+a[i]
+    print(sum)
