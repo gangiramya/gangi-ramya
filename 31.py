e34390dd2cc49bd202db75b078a5564c35fcf0d9
gangiramya <41796378+gangiramya@users.noreply.github.com>
2018-08-21 15:16:29 +0530

    Update 31.py

--- 31.py
+++ 31.py
@@ -1,11 +1,11 @@
 def sumOfAP(A,D,N):
 sum=0
 i=0
-while i<n:
-sum=sum+a
-a=a+d
-i=i+1
-return sum
+  while i<n:
+   sum=sum+a
+   a=a+d
+   i=i+1
+      return sum
 n=int(input(""))
 a=int(input(""))
 d=int(input(""))
